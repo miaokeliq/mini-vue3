10cf636e0880f6260d8bf8210f8ffb89c2402961
miaokeliq <921418076@qq.com>
2023-02-28 07:59:25 +0800

    初步实现 readonly 代码

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -24,3 +24,23 @@ export function reactive(raw: any) {
     },
   });
 }
+
+export function readonly(raw) {
+  return new Proxy(raw, {
+    /*
+     * target: 指向当前获取的对象
+     * ket: 指向用户访问的key
+     * */
+    get(target, key) {
+      // { foo : 1}
+      //
+      const res = Reflect.get(target, key);
+
+      return res;
+    },
+
+    set(target, key, value) {
+      return true;
+    },
+  });
+}
