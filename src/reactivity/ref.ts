a871e3b4396e2663627e73fb4f12146fd9f3e772
miaokeliq <921418076@qq.com>
2023-03-01 08:22:10 +0800

    初步完成当 ref() 传入的是对象 时的代码，并测试通过

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,11 +1,18 @@
-import { hasChanged } from "../shared";
+import { hasChanged, isObject } from "../shared";
 import { isTracking, trackEffects, triggerEffects } from "./effect";
-
+import { reactive } from "./reactive";
 class RefImpl {
   private _value: any;
   public dep; // 保存依赖的  只有一个dep ，因为只有一个_value
+
+  private _rawValue: any;
   constructor(value) {
-    this._value = value;
+    this._rawValue = value;
+    this._value = isObject(value) ? reactive(value) : value;
+
+    // 如果传过来的是对象，需要把value转换成reactive包裹之后的结果，因为所有对象都需要reactive处理
+    // 1. 看看 value 是不是 对象j
+
     this.dep = new Set();
   }
 
@@ -19,9 +26,11 @@ class RefImpl {
 
     // 判断是否set相同的值，如果相同就不触发依赖
     // hasChanged
-    if (hasChanged(newValue, this._value)) {
-      this._value = newValue;
+    // 对比的时候 object , 如果 _value 是对象，则就对比被 reactive 之前的 值 -> _rawValue
+    if (hasChanged(newValue, this._rawValue)) {
+      this._rawValue = newValue;
 
+      this._value = isObject(newValue) ? reactive(newValue) : newValue;
       // 触发依赖
       triggerEffects(this.dep);
     }
