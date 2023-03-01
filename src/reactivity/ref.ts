02f25ce97d503853d6f43d25daa66c3c70c64572
miaokeliq <921418076@qq.com>
2023-03-01 08:29:37 +0800

    重构 ref() 传入的是对象时的代码

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,6 +1,13 @@
 import { hasChanged, isObject } from "../shared";
 import { isTracking, trackEffects, triggerEffects } from "./effect";
 import { reactive } from "./reactive";
+/**
+ *    因为一般 ref 包裹的都是单值，比如： 1 true "1"
+ *    问题就是怎么知道值被 get 或者被 set 了
+ *    这样利用之前的 proxy 就不行了, 因为 proxy 只针对于对象，而包裹的是值类型
+ *    解决方法： 通过对象进行包裹，也就是 RefImpl , 这个 类里面有 value 值，然后给类写 get set，这样就能知道什么时候触发 set 和 get ，就可以做依赖收集和触发依赖了
+ *
+ * */
 class RefImpl {
   private _value: any;
   public dep; // 保存依赖的  只有一个dep ，因为只有一个_value
@@ -8,7 +15,7 @@ class RefImpl {
   private _rawValue: any;
   constructor(value) {
     this._rawValue = value;
-    this._value = isObject(value) ? reactive(value) : value;
+    this._value = convert(value);
 
     // 如果传过来的是对象，需要把value转换成reactive包裹之后的结果，因为所有对象都需要reactive处理
     // 1. 看看 value 是不是 对象j
@@ -30,11 +37,15 @@ class RefImpl {
     if (hasChanged(newValue, this._rawValue)) {
       this._rawValue = newValue;
 
-      this._value = isObject(newValue) ? reactive(newValue) : newValue;
+      this._value = convert(newValue);
       // 触发依赖
       triggerEffects(this.dep);
     }
   }
+}
+// convert->转换j
+function convert(value) {
+  return isObject(value) ? reactive(value) : value;
 }
 
 function trackRefValue(ref) {
