2ef717b9c9009c6ac4a493999f7c03785d609e1d
miaokeliq <921418076@qq.com>
2023-03-02 12:36:59 +0800

    实现初始化 element 主流程

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,5 +1,5 @@
 import { createComponentInstance, setupComponent } from "./component";
-
+import { isObject } from "../shared";
 // render 函数 返回想要渲染的虚拟节点
 export function render(vnode, container) {
   // patch
@@ -14,9 +14,50 @@ function patch(vnode, container) {
   // 判断 是不是 element 类型
   // 是 element 那么就应该处理 element
 
-  // processElement();
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  }
+}
 
-  processComponent(vnode, container);
+function processElement(vnode, container) {
+  // 分为 初始化 和 更新
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode, container) {
+  const el = document.createElement(vnode.type);
+
+  // 给 el.textContent = 'hi mini-vue'
+  //
+  // el.setAttribute("id", "root")
+  //
+  // document.body.append(el)
+
+  // string array
+  const { children } = vnode;
+
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el);
+  }
+  // props
+  const { props } = vnode;
+  for (let key in props) {
+    let val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  container.append(el);
+}
+
+function mountChildren(vnode, container) {
+  // vnode
+  vnode.children.forEach((v) => {
+    patch(v, container);
+  });
 }
 
 function processComponent(vnode: any, container: any) {
